76ee7e5837d2e2393aed32512d75e540e6a31da6
Dave Sanders <davepaulsanders@gmail.com>
2022-05-08 15:37:01 -0400

    Resolved error in instance method placement of user, added Post Model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,12 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 const bcrypt = require("bcrypt");
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
@@ -42,10 +47,6 @@ User.init(
         // this means the password must be at least four characters long
         len: [4],
       },
-    },
-    // set up method to run on instance data (per user) to check password
-    checkPassword(loginPw) {
-      return bcrypt.compareSync(loginPw, this.password);
     },
   },
 
